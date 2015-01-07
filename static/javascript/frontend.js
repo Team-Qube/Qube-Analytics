6c75ddecb1a75bb8b445800141d4955f6a8bfbcc
Paul <bumj94@gmail.com>
2015-01-07 15:17:47 -0500

    update momentjs every 5 seconds

--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -5,7 +5,7 @@ $(document).on('ready', function(){
 		var content="";
 		for(var i = 0; i<data.data.length; i++){
 			var time =moment(new Date(data.data[i].lastLogin));
-			content+="<tr id='"+data.data[i].id+"'><td id='userID'> ID: "+data.data[i].id+"</td><td id='lastLogin'>"+time.fromNow()+"</td><td id='loginCount'>Count: "+data.data[i].loginCount+"</td></tr>";
+			content+="<tr id='"+data.data[i].id+"'><td ='userID'> ID: "+data.data[i].id+"</td><td class='lastLogin' id="+data.data[i].lastLogin+">"+time.fromNow()+"</td><td id='loginCount'>"+data.data[i].loginCount+"</td></tr>";
 		}
 		$('table').append(content);
 	});
@@ -13,9 +13,17 @@ $(document).on('ready', function(){
 	socket.on('update', function(data){
 		console.log('updating...');
 		var time = moment(new Date(data.data.lastLogin));
-		console.log(time);
-		console.log(data.data.loginCount);
-		$('#'+data.data.id + ' td#lastLogin').text(time.fromNow());
+		$('#'+data.data.id + ' td.lastLogin').text(time.fromNow());
+		$('#'+data.data.id + ' td.lastLogin').attr('id', data.data.lastLogin);
 		$('#'+data.data.id + ' td#loginCount').text(data.data.loginCount);
 	});
+
+	setInterval(function(){
+		$('td.lastLogin').map(function(){
+			var time = moment(new Date($(this).attr('id'))).fromNow();
+			if ($(this).text() !== time){
+				$(this).text(time);
+			}
+		});
+	},5000);
 });
